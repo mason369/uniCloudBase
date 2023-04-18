b9d124c05760c0b41f7e67252702b37b49ed762c
mason369 <1960638223@qq.com>
2023-04-18 16:03:41 +0800

    update：添加结构样式

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,56 +1,91 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
-		</view>
-	</view>
+    <view class="home">
+        <view class="content">
+            <view class="item" v-for="item in 5">
+                <view class="text">
+					<view class="title">撒地方撒大发送到发送到发送到发送到发送到噶事的噶十多个的上岗标题</view>
+					<!-- info -->
+					<view class="info">
+						<text>是的</text>
+						<text>2020-23-12</text>
+						<text>删除</text>
+					</view>
+				</view>
+                <view class="pic">
+                    <image
+                        mode="aspectFill"
+                        src="../../static/images/nopic.jpg"
+                    ></image>
+                </view>
+            </view>
+        </view>
+    </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
-			uniCloud.callFunction({
-				name:"uniCloudBase"
-			}).then(r=>{
-				console.log(r);
-			})
-		},
-		methods: {
-
-		}
-	}
+export default {
+    data() {
+        return {
+            title: "Hello",
+        };
+    },
+    onLoad() {
+        uniCloud
+            .callFunction({
+                name: "uniCloudBase",
+            })
+            .then((r) => {
+                console.log(r);
+            });
+    },
+    methods: {},
+};
 </script>
 
-<style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
+<style lang="scss">
+.home {
+    .content {
+        padding: 30rpx;
+        .item {
+            display: flex;
+            justify-content: space-between;
+			padding: 20rpx 0;
+			border-bottom: 1px solid #eee;
+			padding-right: 20rpx;
+            .text {
+                flex: 1;
+				display: flex;
+				flex-direction: column;
+				justify-content: space-between;
+				.title{
+					font-size: 50rpx;
+					color: #333;
+					text-align: justify;
+					// 超出两行显示省略号
+					overflow: hidden;
+					text-overflow: ellipsis;
+					display: -webkit-box;
+					-webkit-line-clamp: 2;
+					-webkit-box-orient: vertical;
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+				}
+				.info {
+					font-size: 28rpx;
+					color: #888;
+					text{
+						padding-right: 20rpx;
+					}
+				}
+            }
+            .pic {
+                width: 260rpx;
+                height: 180rpx;
+                image {
+                    width: 100%;
+                    height: 100%;
+                }
+            }
+        }
+    }
+}
 </style>
